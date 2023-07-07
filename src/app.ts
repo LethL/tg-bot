df971ae5f1bab2af5834dc6b0c34aeb44f7eb99d
Leth4L <carribean_08@mail.ru>
2023-07-07 13:04:07 +0300

    feat: add bot class and bot context

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import { Telegraf, session } from "telegraf";
+import { IConfigService } from "./config/config.interface";
+import { ConfigService } from "./config/config.service";
+import { IBotContext } from './context/context.interface';
+
+class Bot {
+  bot: Telegraf<IBotContext>;
+
+  constructor(private readonly configService: IConfigService) {
+    this.bot = new Telegraf<IBotContext>(this.configService.get("TOKEN"));
+    this.bot.use(session());
+  }
+
+  init() {
+    this.bot.launch();
+  }
+}
+
+const bot = new Bot(new ConfigService());
+bot.init();
+ 
